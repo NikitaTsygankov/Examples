567ac42dec551ebce2bfaaf81462ede9acb3d4d3
Nikita Tsygankov <like_fender@mail.ru>
2023-03-22 21:22:03 +0300

    fix changes

--- number006_What_Your_Name/Program.cs
+++ number006_What_Your_Name/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Введите Ваше имя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "кирилл")
+{
+    Console.Write("Привет, мой ЛЮБИМЫЙ Килька!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
